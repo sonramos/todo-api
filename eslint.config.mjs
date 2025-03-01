4f02ef59591f76e310a30c52d07aeedf3d17610f
Jackson Ramos <jackson.ramoosg@gmail.com>
2025-03-01 15:39:47 -0300

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import globals from "globals";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsp from "@typescript-eslint/parser";
+import js from "@eslint/js";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js", "**/*.ts"],
+    languageOptions: { 
+      sourceType: "module",
+      parser: tsp,
+      globals: globals.node,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "no-console": "off",
+      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }]
+    }
+  }
+];
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const str = 'str';
+
+console.log(str);
+const test = (str: string) => {
+  return str + ' ' + 'eh String no javascript';
+};
+
+console.log(test);
